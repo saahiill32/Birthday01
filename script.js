0939311612433178ea784c15ab902f48da0ef334
saahiill32 <wasnikmanohar8@gmail.com>
2026-01-17 21:23:14 +0530

    Adjust duck background music volume level

--- script.js
+++ script.js
@@ -468,7 +468,7 @@ function setupVoicePlayer({ voiceAudio, bgMusic }) {
 
   function duckBackground(duck) {
     if (!bgMusic) return;
-    fadeVolume(bgMusic, duck ? 0.14 : 0.55, 600);
+    fadeVolume(bgMusic, duck ? 0.10 : 0.55, 600);
   }
 
   voiceAudio.addEventListener("loadedmetadata", () => {
